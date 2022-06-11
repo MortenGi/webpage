476bcc189ca014165ccb993c40934fd28ffafb68
Morten Giese <mortengiese@gmx.de>
2022-06-11 16:50:04 +0200

    add db init

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from views import views
-
+from os import path
 #This is a playground for flask
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix="")
-app.secret_key = 'dasda'
-app.config['SQLALCHEMY_DATA_URI'] = f'sqlite:///{DB_NAME}'
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'dasda'
+    app.config['SQLALCHEMY_DATA_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+    app.register_blueprint(views, url_prefix="")
+
+    from models import User, Note
+    create_db(app)
+    return app
+
+def create_db(app):
+    if not path.exists('webpage/'+ DB_NAME):
+        db.create_all(app=app)
+        print('created database')
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True, port= 8000)
