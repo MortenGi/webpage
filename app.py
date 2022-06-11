d4be856e68dd97c71de95c64e1fb91a5916946ca
Giese, Morten (Mac) <mgiese@mpa-garching.mpg.de>
2022-06-11 18:33:58 +0200

    avoid circular import

--- app.py
+++ app.py
@@ -1,29 +1,7 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from views import views
-from os import path
-#This is a playground for flask
+from website import create_app
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'dasda'
-    app.config['SQLALCHEMY_DATA_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
-    app.register_blueprint(views, url_prefix="")
-
-    from models import User, Note
-    create_db(app)
-    return app
-
-def create_db(app):
-    if not path.exists('webpage/'+ DB_NAME):
-        db.create_all(app=app)
-        print('created database')
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
+
     app.run(debug=True, port= 8000)
